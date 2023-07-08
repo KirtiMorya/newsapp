d71ebd3d432f8db8dd1b827b4eac4c46b7a7f18c
Kirti morya <moryakirti7@gmail.com>
2023-07-08 12:02:03 +0530

    On branch master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+   Routes,
+  Route,
+} from "react-router-dom";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+        <NavBar/>
+        <Routes>
+          <Route path="/Home" element={< News  key='general' pageSize={6} country="in" category="general"/>}></Route>
+          <Route path="/Business" element={ <News key='business'  pageSize={6} country="in" category="business"/>}></Route>
+          <Route path="/Entertainment" element={<News key='entertainment'  pageSize={6} country="in" category="entertainment"/>}></Route>
+          <Route path="/Health" element={ <News key='health'  pageSize={6} country="in" category="health"/>}></Route>
+          <Route path="/Science" element={ <News key='science'  pageSize={6} country="in" category="science"/>}></Route>
+          <Route path="/Sports" element={ <News key='sports'  pageSize={6} country="in" category="sports"/>}></Route>
+          <Route path="/Technology" element={ <News  key='technology}>' pageSize={6} country="in" category="technology"/>}></Route>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
